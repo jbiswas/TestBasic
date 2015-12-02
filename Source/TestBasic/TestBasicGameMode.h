0ca64f9d79d8ff77012a0c2b34060ef133f9a38e
Jayant Biswas <biswasj@gmail.com>
2015-12-02 16:39:23 +0100

    Add other source files that may be required for the project

---
+++ Source/TestBasic/TestBasicGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TestBasicGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTBASIC_API ATestBasicGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
